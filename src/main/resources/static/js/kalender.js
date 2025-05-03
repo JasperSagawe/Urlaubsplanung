19d1fdd0a2a501089cb2cd0d8b0b6808ddddef92
Jasper Sagawe <139363352+JasperSagawe@users.noreply.github.com>
2025-05-03 11:59:36 +0200

    Erweitere den Kalender um einen Dialog zur Urlaubseinreichung

--- src/main/resources/static/js/kalender.js
+++ src/main/resources/static/js/kalender.js
@@ -1,7 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
   var calendarEl = document.getElementById("kalender");
+  var dialog = document.getElementById("eventDialog");
+  var form = document.getElementById("eventForm");
   var calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: "dayGridMonth",
+    dateClick: function (info) {
+      dialog.showModal();
+      form.startDate.value = info.dateStr;
+      form.endDate.value = info.dateStr;
+    },
   });
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var title = form.eventName.value;
+    var start = form.startDate.value;
+    var end = form.endDate.value;
+
+    if (title && start) {
+      calendar.addEvent({
+        title: title,
+        start: start,
+        end: end,
+      });
+    }
+
+    dialog.close();
+    form.reset();
+  });
+
+  document.getElementById("cancel").addEventListener("click", function () {
+    dialog.close();
+  });
+
   calendar.render();
 });
