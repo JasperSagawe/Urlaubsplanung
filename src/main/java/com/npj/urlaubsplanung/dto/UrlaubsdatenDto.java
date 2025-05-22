3983f29926c6ea5598f6b8e0069cc44dae38a35d
Jasper Sagawe <139363352+JasperSagawe@users.noreply.github.com>
2025-05-22 12:40:35 +0200

    Urlaubsdaten anpassen

--- src/main/java/com/npj/urlaubsplanung/dto/UrlaubsdatenDto.java
+++ src/main/java/com/npj/urlaubsplanung/dto/UrlaubsdatenDto.java
@@ -4,48 +4,19 @@ import java.util.List;
 
 public class UrlaubsdatenDto {
 
-	private String vorname;
-	private String nachname;
 	private int urlaubVorjahr;
 	private int offeneUrlaubstage;
 	private int beantragteUrlaubstage;
 	private int genommeneUrlaubstage;
 	private List<UrlaubstagDto> urlaubstage;
 
-	public UrlaubsdatenDto(String vorname, String nachname, int urlaubVorjahr, int offeneUrlaubstage) {
-		this.vorname = vorname;
-		this.nachname = nachname;
-		this.urlaubVorjahr = urlaubVorjahr;
-		this.offeneUrlaubstage = offeneUrlaubstage;
-		this.beantragteUrlaubstage = 0;
-		this.genommeneUrlaubstage = 0;
-	}
-
-	public UrlaubsdatenDto(String vorname, String nachname, int urlaubVorjahr, int offeneUrlaubstage,
+	public UrlaubsdatenDto(int urlaubVorjahr, int offeneUrlaubstage,
 			int beantragteUrlaubstage, int genommeneUrlaubstage, List<UrlaubstagDto> urlaubstage) {
-		this.vorname = vorname;
-		this.nachname = nachname;
 		this.urlaubVorjahr = urlaubVorjahr;
 		this.offeneUrlaubstage = offeneUrlaubstage;
 		this.beantragteUrlaubstage = beantragteUrlaubstage;
 		this.genommeneUrlaubstage = genommeneUrlaubstage;
 		this.urlaubstage = urlaubstage;
-	}
-
-	public String getVorname() {
-		return vorname;
-	}
-
-	public void setVorname(String vorname) {
-		this.vorname = vorname;
-	}
-
-	public String getNachname() {
-		return nachname;
-	}
-
-	public void setNachname(String nachname) {
-		this.nachname = nachname;
 	}
 
 	public int getUrlaubVorjahr() {
