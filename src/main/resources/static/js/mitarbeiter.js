8daa2c5b69bd402b9fa4b931b86d0b105743dc9e
Jasper Sagawe <139363352+JasperSagawe@users.noreply.github.com>
2025-05-30 18:16:22 +0200

    Ermögliche das Bearbeiten eines Mitarbeiters

--- src/main/resources/static/js/mitarbeiter.js
+++ src/main/resources/static/js/mitarbeiter.js
@@ -13,13 +13,23 @@ document.addEventListener("DOMContentLoaded", function () {
   const abteilungSelect = document.getElementById("abteilung-select");
   const rolleSelect = document.getElementById("rolle-select");
 
-  if (anlegenBtn && dialog && abbrechenBtn && form) {
-    anlegenBtn.addEventListener("click", () => dialog.showModal());
-    abbrechenBtn.addEventListener("click", () => dialog.close());
+  let mitarbeiterId = null;
 
+  if (anlegenBtn && dialog && abbrechenBtn && form) {
+    anlegenBtn.addEventListener("click", () => {
+      mitarbeiterId = null;
+      form.reset();
+      ladeAbteilungUndRolleSelect();
+      dialog.showModal();
+    });
+    abbrechenBtn.addEventListener("click", () => dialog.close());
+  }
+
+  if (form) {
     form.addEventListener("submit", function (e) {
       e.preventDefault();
       const data = {
+        id: mitarbeiterId ?? null,
         vorname: form.vorname.value,
         nachname: form.nachname.value,
         passwort: form.passwort.value,
@@ -34,19 +44,14 @@ document.addEventListener("DOMContentLoaded", function () {
           name: form.rolle.options[form.rolle.selectedIndex].text,
         },
       };
-      const headers = {
-        "Content-Type": "application/json",
-      };
-      if (csrfHeader && csrfToken) {
-        headers[csrfHeader] = csrfToken;
-      }
       fetch("/verwaltung/mitarbeiter/save", {
         method: "POST",
-        headers: headers,
+        headers: getHeaders(),
         body: JSON.stringify(data),
       })
         .then((response) => {
           if (response.ok) {
+            mitarbeiterId = null;
             ladeTabelleNeu();
             dialog.close();
             form.reset();
@@ -58,53 +63,59 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  if (anlegenBtn && abteilungSelect) {
-    anlegenBtn.addEventListener("click", function () {
-      fetch("/verwaltung/abteilung-select")
-        .then((response) => response.json())
-        .then((data) => {
-          abteilungSelect.innerHTML =
-            '<option value="">Bitte wählen...</option>';
-          data.forEach((abteilung) => {
-            const option = document.createElement("option");
-            option.value = abteilung.id;
-            option.textContent = abteilung.name;
-            abteilungSelect.appendChild(option);
-          });
-        });
-    });
-  }
+  document.body.addEventListener("click", function (e) {
+    if (e.target.classList.contains("bearbeite-mitarbeiter-btn")) {
+      (mitarbeiterId = e.target.getAttribute("data-id")),
+        (form.vorname.value = e.target.getAttribute("data-vorname"));
+      form.nachname.value = e.target.getAttribute("data-nachname");
+      form.passwort.value = e.target.getAttribute("data-passwort");
+      form.urlaubstageProJahr.value = e.target.getAttribute(
+        "data-urlaubstageProJahr"
+      );
+      ladeAbteilungUndRolleSelect(
+        e.target.getAttribute("data-abteilung-id"),
+        e.target.getAttribute("data-rolle-id")
+      );
+      dialog.showModal();
+    }
+  });
 
-  if (anlegenBtn && rolleSelect) {
-    anlegenBtn.addEventListener("click", function () {
-      fetch("/verwaltung/rolle-select")
-        .then((response) => response.json())
-        .then((data) => {
-          rolleSelect.innerHTML = '<option value="">Bitte wählen...</option>';
-          data.forEach((rolle) => {
-            const option = document.createElement("option");
-            option.value = rolle.id;
-            option.textContent = rolle.name;
-            rolleSelect.appendChild(option);
-          });
+  function ladeAbteilungUndRolleSelect(abteilungId, rolleId) {
+    console.log(abteilungId, rolleId);
+    fetch("/verwaltung/abteilung-select")
+      .then((response) => response.json())
+      .then((data) => {
+        abteilungSelect.innerHTML = '<option value="">Bitte wählen...</option>';
+        data.forEach((abteilung) => {
+          const option = document.createElement("option");
+          option.value = abteilung.id;
+          option.textContent = abteilung.name;
+          option.selected = abteilungId == abteilung.id;
+          abteilungSelect.appendChild(option);
         });
-    });
+      });
+
+    fetch("/verwaltung/rolle-select")
+      .then((response) => response.json())
+      .then((data) => {
+        rolleSelect.innerHTML = '<option value="">Bitte wählen...</option>';
+        data.forEach((rolle) => {
+          const option = document.createElement("option");
+          option.value = rolle.id;
+          option.textContent = rolle.name;
+          option.selected = rolleId == rolle.id;
+          rolleSelect.appendChild(option);
+        });
+      });
   }
 
   document.body.addEventListener("click", function (e) {
     if (e.target.classList.contains("delete-mitarbeiter-btn")) {
       if (e.target.dataset.confirming === "true") {
-        const mitarbeiterId = e.target.getAttribute("data-id");
-        const headers = {
-          "Content-Type": "application/json",
-        };
-        if (csrfHeader && csrfToken) {
-          headers[csrfHeader] = csrfToken;
-        }
-
+        mitarbeiterId = e.target.getAttribute("data-id");
         fetch("/verwaltung/mitarbeiter/delete/" + mitarbeiterId, {
           method: "DELETE",
-          headers: headers,
+          headers: getHeaders(),
         }).then((response) => {
           if (response.ok) {
             ladeTabelleNeu();
@@ -134,8 +145,13 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("mitarbeiter-tabelle-container").innerHTML =
           html;
       })
-      .catch((error) => {
-        console.error("Fehler beim Laden der Tabelle:", error);
-      });
+      .catch((error) => console.error("Fehler beim Laden der Tabelle:", error));
+  }
+
+  function getHeaders() {
+    const headers = { "Content-Type": "application/json" };
+    if (csrfHeader && csrfToken) headers[csrfHeader] = csrfToken;
+
+    return headers;
   }
 });
